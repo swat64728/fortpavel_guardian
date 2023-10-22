ba54941ebcaaa509a340f43c6ba97a41a7e27a16
swat64728 <103403590+swat64728@users.noreply.github.com>
2023-10-22 15:28:17 +0300

    Add files via upload

---
+++ javascript/settings.js
@@ -0,0 +1,7 @@
+var backgroundColor = "rgb(25, 25, 25)";
+var textColor = "rgb(255, 100, 0)";
+var topbarColor = "rgb(50, 50, 50)";
+
+document.documentElement.style.setProperty('--primary-color', backgroundColor);
+document.documentElement.style.setProperty('--secondary-color', textColor);
+document.documentElement.style.setProperty('--thirtiary-color', topbarColor);
